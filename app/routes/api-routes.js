d93283028c39b9bfb83b397f14f4f8555f53e0f1
Modern Sapien <joncanales2@gmail.com>
2020-10-11 10:43:18 -0400

    post route for creating new users is working.

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -10,12 +10,12 @@ const cogsModel = require("../models/cogsModel.js");
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // users api get route
-  app.get("/api/all", function(req, res) {
-    cogsModel.User.findAll(req.body).then((result) =>  {
-      res.json(result)
-  });
-  })
+  // Is there a way to display all objects at once? is that useful?
+  // app.get("/api/all", function(req, res) {
+  //   cogsModel.findAll(req.body).then((result) =>  {
+  //     res.json(result)
+  // });
+  // })
 
   // users api get route
   app.get("/api/users", function(req, res) {
@@ -66,59 +66,18 @@ module.exports = function(app) {
       },
     }).then((result) => {
       res.json(result)
-    } )
+    })
   });
 
-}
-//   // Add sequelize code to get all books of a specific genre and return them as JSON
-//   app.get("/api/genre/:genre", function(req, res) {
-//     Book.findAll({
-//       where: {
-//         title: req.params.genre,
-//       },
-//     }).then((result) => {
-//       res.json(result)
-//     } )
-//   });
-
-//   // Add sequelize code to get all books from a specific author and return them as JSON
-//   app.get("/api/author/:author", function(req, res) {
-//     Book.findAll({
-//       where: {
-//         title: req.params.author,
-//       },
-//     }).then((result) => {
-//       res.json(result)
-//     } )
-//   });
-  
-
-//   // Add sequelize code to get all "long" books (more than 150 pages) and return them as JSON WHRE CLAUSE
-//   app.get("/api/books/long", function(req, res) {
-//     Book.findAll({
-//       where:  {
-//         pages:  {
-//           [Op.gt]:  15
-//         }
-//       }
-//     })
-//   });
-
-//   // Add sequelize code to get all "short" books (150 pages or less) and return them as JSON
-//   app.get("/api/books/short", function(req, res) {
-
-//   });
-
-//   // Add sequelize code to create a book
-//   app.post("/api/new", function(req, res) {
-//     Book.create(req.body).then((result) =>  {
-//       res.json(result)
-//     })
-//   });
+    // Add New User Post
+    app.post("/api/users", function(req, res) {
+      cogsModel.User.create(req.body).then((result) =>  {
+        res.json(result)
+      })
+    });
 
 //   // Add sequelize code to delete a book
 //   app.delete("/api/book/:id", function(req, res) {
 
 //   });
-
-// }
+}
